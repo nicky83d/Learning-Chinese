fd6eb21b0f292c629c4dd3dd21bf08e0cc50142d
nickyd83 <nicodegrandy@gmail.com>
2026-02-14 09:37:16 +0000

    Fix: language selection only shows if 2+ enabled non-English languages (always hidden for 1)

--- static/practice_modals.js
+++ static/practice_modals.js
@@ -1,11 +1,12 @@
 // practice_modals.js
 // Hide language selection if only one non-English language is enabled
 function updatePracticeLanguageSelectors(enabledLangs) {
-  // enabledLangs: array of non-English languages enabled, e.g. ['chinese'], ['chinese','japanese']
+  // enabledLangs: array of non-English languages enabled, e.g. ['chinese'], ['chinese','french']
+  // Hide language selection unless 2 or more enabled non-English languages
   // Practice Words
   const pwLangArea = document.getElementById('pw_lang_select_area');
   if (pwLangArea) {
-    if (enabledLangs.length <= 1) {
+    if (enabledLangs.length < 2) {
       pwLangArea.style.display = 'none';
       if (enabledLangs.length === 1) {
         document.querySelector('input[name="pw_lang"][value="'+enabledLangs[0]+'"]').checked = true;
@@ -17,7 +18,7 @@ function updatePracticeLanguageSelectors(enabledLangs) {
   // Practice Listening
   const plLangArea = document.getElementById('pl_lang_options');
   if (plLangArea) {
-    if (enabledLangs.length <= 1) {
+    if (enabledLangs.length < 2) {
       plLangArea.parentElement.style.display = 'none';
       if (enabledLangs.length === 1) {
         document.querySelector('input[name="pl_lang"][value="'+enabledLangs[0]+'"]').checked = true;
@@ -29,7 +30,7 @@ function updatePracticeLanguageSelectors(enabledLangs) {
   // Practice Speaking
   const ptLangArea = document.getElementById('pt_lang_options');
   if (ptLangArea) {
-    if (enabledLangs.length <= 1) {
+    if (enabledLangs.length < 2) {
       ptLangArea.parentElement.style.display = 'none';
       if (enabledLangs.length === 1) {
         document.querySelector('input[name="pt_lang"][value="'+enabledLangs[0]+'"]').checked = true;
