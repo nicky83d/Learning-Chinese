41ba70d6fec177d49f40bd431d95bfb950d52175
nickyd83 <nicodegrandy@gmail.com>
2026-02-14 09:42:02 +0000

    Fix: show language selection if 2 or more enabled non-English languages (including all enabled)

--- static/practice_modals.js
+++ static/practice_modals.js
@@ -6,37 +6,37 @@ function updatePracticeLanguageSelectors(enabledLangs) {
   // Practice Words
   const pwLangArea = document.getElementById('pw_lang_select_area');
   if (pwLangArea) {
-    if (enabledLangs.length < 2) {
+    if (enabledLangs.length >= 2) {
+      pwLangArea.style.display = '';
+    } else {
       pwLangArea.style.display = 'none';
       if (enabledLangs.length === 1) {
         document.querySelector('input[name="pw_lang"][value="'+enabledLangs[0]+'"]').checked = true;
       }
-    } else {
-      pwLangArea.style.display = '';
     }
   }
   // Practice Listening
   const plLangArea = document.getElementById('pl_lang_options');
   if (plLangArea) {
-    if (enabledLangs.length < 2) {
+    if (enabledLangs.length >= 2) {
+      plLangArea.parentElement.style.display = '';
+    } else {
       plLangArea.parentElement.style.display = 'none';
       if (enabledLangs.length === 1) {
         document.querySelector('input[name="pl_lang"][value="'+enabledLangs[0]+'"]').checked = true;
       }
-    } else {
-      plLangArea.parentElement.style.display = '';
     }
   }
   // Practice Speaking
   const ptLangArea = document.getElementById('pt_lang_options');
   if (ptLangArea) {
-    if (enabledLangs.length < 2) {
+    if (enabledLangs.length >= 2) {
+      ptLangArea.parentElement.style.display = '';
+    } else {
       ptLangArea.parentElement.style.display = 'none';
       if (enabledLangs.length === 1) {
         document.querySelector('input[name="pt_lang"][value="'+enabledLangs[0]+'"]').checked = true;
       }
-    } else {
-      ptLangArea.parentElement.style.display = '';
     }
   }
 }
